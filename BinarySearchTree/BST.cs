636dbd510fe2b01934b2009d7b4b3543ceea31b6
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-29 17:38:37 +0530

    [shweta] added the of Binary Tree

---
+++ BinarySearchTree/BST.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        /// <summary>
+        /// data = user input  
+        /// Node having a data , Left and Right 
+        /// </summary>
+        public int Data;
+        public Node Left;
+        public Node Right;
+
+        public Node(int New_Data)
+        {
+            Data = New_Data;  
+            Left = null;
+            Right = null;
+
+        }
+
+    }
+    class BST
+    {
+        public Node root;
+        public void Insert(int new_data)
+        {
+            Node newNode = new Node(new_data); // creating new Node
+            
+            //check whetre tree is empty
+            //if there is empty ,then set root to newNode
+            if (root == null)
+                root = newNode;
+            else
+            {
+                //if there is not empty then check whether the
+                //value is smaller or larger
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (new_data < current.Data) //if new_data is smaller then move to left child 
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+                    }
+
+                    else
+                    { // else move to right childe side
+                        current = current.Right;
+                        if (current == null)
+                        {
+                            parent.Right = newNode;
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        void inorderRec(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+            else
+            {
+                inorderRec(root.Left);
+                Console.WriteLine(root.Data);
+                inorderRec(root.Right);
+            }
+        }
+       public void inorder()
+        {
+            inorderRec(root);
+        }
+
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BST tree = new BST();
+            tree.Insert(50);
+            tree.Insert(30);
+            tree.Insert(70);
+            Console.WriteLine(" After insert node the Binary Tree is:");
+            tree.inorder();
+        }
+    }
+}
