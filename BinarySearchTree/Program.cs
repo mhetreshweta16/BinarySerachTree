5d1123146a768dba710da8c53e262d54f1d7a3f1
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-29 17:49:17 +0530

    [shweta] added the second UC of Binary Tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,6 +11,17 @@ namespace BinarySearchTree
             tree.Insert(50);
             tree.Insert(30);
             tree.Insert(70);
+            tree.Insert(22);
+            tree.Insert(60);
+            tree.Insert(40);
+            tree.Insert(95);
+            tree.Insert(67);
+            tree.Insert(65);
+            tree.Insert(11);
+            tree.Insert(3);
+            tree.Insert(16);
+            tree.Insert(63);
+
             Console.WriteLine(" After insert node the Binary Tree is:");
             tree.inorder();
         }
